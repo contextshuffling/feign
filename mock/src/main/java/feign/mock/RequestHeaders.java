f3e2376cb1c26283df4a9d121a8e601a2f5ce1e6
contextshuffling <contextshuffling@gmail.com>
2020-01-28 08:52:04 -0600

    Use LinkedHashMap for deterministic iterations

--- mock/src/main/java/feign/mock/RequestHeaders.java
+++ mock/src/main/java/feign/mock/RequestHeaders.java
@@ -19,103 +19,103 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.*;
 
 public class RequestHeaders {
 
-  public static class Builder {
+    public static class Builder {
 
-    private Map<String, Collection<String>> headers = new HashMap<String, Collection<String>>();
+        private Map<String, Collection<String>> headers = new LinkedHashMap<String, Collection<String>>();
 
-    private Builder() {}
+        private Builder() {
+        }
 
-    public Builder add(String key, Collection<String> values) {
-      if (!headers.containsKey(key)) {
-        headers.put(key, values);
-      } else {
-        Collection<String> previousValues = headers.get(key);
-        previousValues.addAll(values);
-        headers.put(key, previousValues);
-      }
-      return this;
+        public Builder add(String key, Collection<String> values) {
+            if (!headers.containsKey(key)) {
+                headers.put(key, values);
+            } else {
+                Collection<String> previousValues = headers.get(key);
+                previousValues.addAll(values);
+                headers.put(key, previousValues);
+            }
+            return this;
+        }
+
+        public Builder add(String key, String value) {
+            if (!headers.containsKey(key)) {
+                headers.put(key, new ArrayList<String>(Arrays.asList(value)));
+            } else {
+                final Collection<String> values = headers.get(key);
+                values.add(value);
+                headers.put(key, values);
+            }
+            return this;
+        }
+
+        public RequestHeaders build() {
+            return new RequestHeaders(this);
+        }
     }
 
-    public Builder add(String key, String value) {
-      if (!headers.containsKey(key)) {
-        headers.put(key, new ArrayList<String>(Arrays.asList(value)));
-      } else {
-        final Collection<String> values = headers.get(key);
-        values.add(value);
-        headers.put(key, values);
-      }
-      return this;
+    public static final Map<String, Collection<String>> EMPTY = Collections.emptyMap();
+
+    public static Builder builder() {
+        return new Builder();
     }
 
-    public RequestHeaders build() {
-      return new RequestHeaders(this);
+    public static RequestHeaders of(Map<String, Collection<String>> headers) {
+        return new RequestHeaders(headers);
     }
 
-  }
+    private Map<String, Collection<String>> headers;
 
-  public static final Map<String, Collection<String>> EMPTY = Collections.emptyMap();
-
-  public static Builder builder() {
-    return new Builder();
-  }
-
-  public static RequestHeaders of(Map<String, Collection<String>> headers) {
-    return new RequestHeaders(headers);
-  }
-
-  private Map<String, Collection<String>> headers;
-
-  private RequestHeaders(Builder builder) {
-    this.headers = builder.headers;
-  }
-
-  private RequestHeaders(Map<String, Collection<String>> headers) {
-    this.headers = headers;
-  }
-
-  public int size() {
-    return headers.size();
-  }
-
-  public int sizeOf(String key) {
-    if (!headers.containsKey(key)) {
-      return 0;
+    private RequestHeaders(Builder builder) {
+        this.headers = builder.headers;
     }
-    return headers.get(key).size();
-  }
 
-  public Collection<String> fetch(String key) {
-    return headers.get(key);
-  }
+    private RequestHeaders(Map<String, Collection<String>> headers) {
+        this.headers = headers;
+    }
 
-  @Override
-  public boolean equals(Object obj) {
-    if (this == obj) {
-      return true;
+    public int size() {
+        return headers.size();
     }
-    if (obj == null) {
-      return false;
-    }
-    if (getClass() != obj.getClass()) {
-      return false;
-    }
-    final RequestHeaders other = (RequestHeaders) obj;
-    return this.headers.equals(other.headers);
-  }
 
-  @Override
-  public String toString() {
-    StringBuilder builder = new StringBuilder();
-    for (Map.Entry<String, Collection<String>> entry : headers.entrySet()) {
-      builder.append(entry).append(',').append(' ');
+    public int sizeOf(String key) {
+        if (!headers.containsKey(key)) {
+            return 0;
+        }
+        return headers.get(key).size();
     }
-    if (builder.length() > 0) {
-      return builder.substring(0, builder.length() - 2);
-    }
-    return "no";
-  }
 
+    public Collection<String> fetch(String key) {
+        return headers.get(key);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final RequestHeaders other = (RequestHeaders) obj;
+        return this.headers.equals(other.headers);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        for (Map.Entry<String, Collection<String>> entry : headers.entrySet()) {
+            builder.append(entry).append(',').append(' ');
+        }
+        if (builder.length() > 0) {
+            return builder.substring(0, builder.length() - 2);
+        }
+        return "no";
+    }
 }
--- mock/src/test/java/feign/mock/RequestHeadersTest.java
+++ mock/src/test/java/feign/mock/RequestHeadersTest.java
@@ -83,6 +83,6 @@ public class RequestHeadersTest {
         .add("other header", "val2")
         .add("header", Arrays.asList("val3", "val4"))
         .build();
-    assertThat(headers.toString()).isEqualTo("other header=[val2], header=[val, val3, val4]");
+    assertThat(headers.toString()).isEqualTo("header=[val, val3, val4], other header=[val2]");
   }
 }
